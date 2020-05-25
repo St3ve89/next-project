8169a60f9f5c2fadfca11db9afebf4ba1b5990dc
Istvan Acs <istvan.acs89@gmail.com>
2020-05-25 18:22:43 +0100

    Remove unused files

--- pages/blogs.js
+++
@@ -1,9 +0,0 @@
-import BaseLayout from '../components/layouts/BaseLayout';
-
-const Blogs = () => (
-  <BaseLayout>
-    <h1>I am Blogs page</h1>
-  </BaseLayout>
-);
-
-export default Blogs;
--- pages/cv.js
+++
@@ -1,8 +0,0 @@
-import BaseLayout from '../components/layouts/BaseLayout';
-
-const CV = () => (
-  <BaseLayout>
-    <h1>I am CV page</h1>;
-  </BaseLayout>
-);
-export default CV;

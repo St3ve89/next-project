91c44f795f96ff760bf21c05ba3357f573c1cf5f
Istvan Acs <istvan.acs89@gmail.com>
2020-06-06 10:51:19 +0100

    Get portfolios by ID

---
+++ pages/portfolio.js
@@ -0,0 +1,9 @@
+import BaseLayout from '../components/layouts/BaseLayout';
+
+const Portfolio = () => (
+  <BaseLayout>
+    <h1>I am Portfolio page!!!!</h1>
+  </BaseLayout>
+);
+
+export default Portfolio;

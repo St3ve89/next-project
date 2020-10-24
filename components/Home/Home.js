c8a81eeb52ebb5fe99e74c0188a1f546a5637d18
Istvan Acs <istvan.acs89@gmail.com>
2020-10-24 18:09:46 +0100

    Add image and make home page responsive

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -6,34 +6,37 @@ const Home = () => (
       <div className='home-container'>
         <div className='home-item'>
           <div className='home-info'>
-            <div className='home-promo'>
+            <div className='home-promo fadeInUp'>
               <span>Hello I'm</span>
             </div>
-            <h2>Istvan Acs</h2>
-            <h4>Software Engineer</h4>
-            <ul className='social-icons'>
+            <h2 className='fadeInUp delay02'>Istvan Acs</h2>
+            <h4 className='fadeInUp delay03'>Software Engineer</h4>
+            <ul className='social-icons fadeInUp delay07 '>
               <li>
-                <div className='fa'>
-                  <a href={'https://github.com/St3ve89'} target='_blank'>
-                    <FaGithubAlt />
-                  </a>
-                </div>
+                <a
+                  className='fa'
+                  href={'https://github.com/St3ve89'}
+                  target='_blank'
+                >
+                  <FaGithubAlt />
+                </a>
               </li>
               <li>
-                <div className='fa'>
-                  <a
-                    href={'https://www.linkedin.com/in/istvan-acs-b24479160/'}
-                    target='_blank'
-                  >
-                    <FaLinkedin />
-                  </a>
-                </div>
+                <a
+                  className='fa'
+                  href={'https://www.linkedin.com/in/istvan-acs-b24479160/'}
+                  target='_blank'
+                >
+                  <FaLinkedin />
+                </a>
               </li>
             </ul>
           </div>
         </div>
         <div className='home-item'>
-          <div className='home-hero-img'></div>
+          <div className='home-hero-img fadeInUp delay08'>
+            <img src='/images/IMG_E98602.jpg' alt='Image of myself' />
+          </div>
         </div>
       </div>
     </div>

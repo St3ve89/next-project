0deff5b0f4802e13f8fd098a03e99542566afb21
Istvan Acs <istvan.acs89@gmail.com>
2020-10-24 20:51:41 +0100

    Add fade in effect to links

--- components/shared/Header/Header.js
+++ components/shared/Header/Header.js
@@ -16,17 +16,17 @@ const Header = () => {
             <img src='/images/logo.png' alt='logo' />
           </div>
           <ul className={`${open ? 'open' : ''} navbar`}>
-            <li>
+            <li className={`${open ? 'fade' : ''}`}>
               <Link href='/'>
                 <a>Home</a>
               </Link>
             </li>
-            <li>
+            <li className={`${open ? 'fade' : ''}`}>
               <Link href='/about'>
                 <a>About</a>
               </Link>
             </li>
-            <li>
+            <li className={`${open ? 'fade' : ''}`}>
               <Link href='/portfolios'>
                 <a>Portfolios</a>
               </Link>

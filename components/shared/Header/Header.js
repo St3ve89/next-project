87fe8ca8da0f6b6c625baaeb783a2a49c7588efc
Istvan Acs <istvan.acs89@gmail.com>
2020-10-24 18:10:12 +0100

    Add responsiveness to the header component WIP

--- components/shared/Header/Header.js
+++ components/shared/Header/Header.js
@@ -5,24 +5,27 @@ const Header = () => (
     <div className='container'>
       <nav>
         <div className='logo'>Logo</div>
-        <div className='navbar'>
-          <ul>
-            <li>
-              <Link href='/'>
-                <a>Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/about'>
-                <a>About</a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/portfolios'>
-                <a>Portfolios</a>
-              </Link>
-            </li>
-          </ul>
+        <ul className='navbar'>
+          <li>
+            <Link href='/'>
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/about'>
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/portfolios'>
+              <a>Portfolios</a>
+            </Link>
+          </li>
+        </ul>
+        <div className='burger'>
+          <div className='line1'></div>
+          <div className='line2'></div>
+          <div className='line3'></div>
         </div>
       </nav>
     </div>

c3eebf6f2500dc01ce63a6d986748dff1f53bab6
Istvan Acs <istvan.acs89@gmail.com>
2020-10-24 20:32:50 +0100

    Add responsivness to header

--- components/shared/Header/Header.js
+++ components/shared/Header/Header.js
@@ -1,35 +1,44 @@
+import { useState } from 'react';
 import Link from '../../Link/Link';
 
-const Header = () => (
-  <header>
-    <div className='container'>
-      <nav>
-        <div className='logo'>Logo</div>
-        <ul className='navbar'>
-          <li>
-            <Link href='/'>
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/about'>
-              <a>About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/portfolios'>
-              <a>Portfolios</a>
-            </Link>
-          </li>
-        </ul>
-        <div className='burger'>
-          <div className='line1'></div>
-          <div className='line2'></div>
-          <div className='line3'></div>
-        </div>
-      </nav>
-    </div>
-  </header>
-);
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleNavClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <header>
+      <div className='container'>
+        <nav>
+          <div className='logo'>Logo</div>
+          <ul className={`${open ? 'open' : ''} navbar`}>
+            <li>
+              <Link href='/'>
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/about'>
+                <a>About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/portfolios'>
+                <a>Portfolios</a>
+              </Link>
+            </li>
+          </ul>
+          <button onClick={handleNavClick} className='burger'>
+            <div className='line'></div>
+            <div className='line'></div>
+            <div className='line'></div>
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;

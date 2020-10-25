c2eba01db796e6207c0bde3dc4b4d6032e7bd23d
Istvan Acs <istvan.acs89@gmail.com>
2020-10-25 14:41:29 +0000

    Create about section WIP

---
+++ components/About/About.js
@@ -0,0 +1,9 @@
+const About = () => (
+  <section className='about' id='about'>
+    <div className='container'>
+      <h1>This is about me section</h1>
+    </div>
+  </section>
+);
+
+export default About;
